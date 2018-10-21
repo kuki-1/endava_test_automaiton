9c1d1bc2ae77a6796c8c81e0a06326572ed5b8ad
Jelena Corak <jelena.v.corak@gmail.com>
2018-10-21 12:39:49 +0200

    F5 author name format changed

--- src/main/java/com/endava/util/Utils.java
+++ src/main/java/com/endava/util/Utils.java
@@ -1,12 +1,13 @@
 package com.endava.util;
 
-import com.endava.pages.HomePage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import com.endava.pages.HomePage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * @author Vladimir Krekic
@@ -14,36 +15,38 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Utils {
 
-    /**
-     * @author Vladimir Krekic
-     * @param browser String that represents chosen browser from testng.xml file
-     * @return HomePage
-     */
-    public static HomePage setUpWebBrowser(String browser){
-        HomePage homePage;
-        if(browser.equalsIgnoreCase("chrome")){
-            WebDriverManager.chromedriver().setup();
-            homePage = new HomePage(new ChromeDriver(disableInfobarsOption()));
-        }else if(browser.equalsIgnoreCase("firefox")){
-            WebDriverManager.firefoxdriver().setup();
-            homePage = new HomePage(new FirefoxDriver());
-        }else if(browser.equalsIgnoreCase("internet explorer")){
-            WebDriverManager.iedriver().setup();
-            homePage = new HomePage(new InternetExplorerDriver());
-        }else throw new RuntimeException();
+	/**
+	 * @author Vladimir Krekic
+	 * @param browser
+	 *            String that represents chosen browser from testng.xml file
+	 * @return HomePage
+	 */
+	public static HomePage setUpWebBrowser(String browser) {
+		HomePage homePage;
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			homePage = new HomePage(new ChromeDriver(disableInfobarsOption()));
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			homePage = new HomePage(new FirefoxDriver());
+		} else if (browser.equalsIgnoreCase("internet explorer")) {
+			WebDriverManager.iedriver().setup();
+			homePage = new HomePage(new InternetExplorerDriver());
+		} else
+			throw new RuntimeException();
 
-        return homePage;
-    }
-    
-    /**
-     * Returns option that disables infobars in chrome browser
-     *
-     * @author jelena.v.corak@gmail.com
-     * @return ChromeOptions
-     */
-    public static ChromeOptions disableInfobarsOption() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-infobars");
-        return options;
-    }
+		return homePage;
+	}
+
+	/**
+	 * Returns option that disables infobars in chrome browser
+	 *
+	 * @author jelena.corak
+	 * @return ChromeOptions
+	 */
+	public static ChromeOptions disableInfobarsOption() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-infobars");
+		return options;
+	}
 }
