c9a4bfa41b4e23ab9305cde3d797e5d3726dd6c9
jcorak-endava <jelena.corak@endava.com>
2018-10-22 19:59:50 +0200

    T3 changes: page title and URL verification added after each new page. Error detected at Investors page - Investors expected, Endava - Home found!

--- src/test/java/com/endava/TestMenuPage.java
+++ src/test/java/com/endava/TestMenuPage.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import com.endava.pages.HomePage;
 import com.endava.pages.MenuPage;
 import com.endava.util.Utils;
@@ -15,19 +16,20 @@ import com.endava.util.Utils;
  *
  */
 public class TestMenuPage {
-	
+
 	private HomePage homePage;
 	private MenuPage menuPage;
 	private static Logger log = Logger.getLogger(TestMenuPage.class);
 
 	/**
 	 * @author jelena.corak
-	 * @param browser web browser defined in testng.xml
+	 * @param browser
+	 *            web browser defined in testng.xml
 	 */
 	@BeforeMethod
 	@Parameters({ "browser" })
 	public void setUp(String browser) {
-		homePage = Utils.setUpWebBrowser(browser);		
+		homePage = Utils.setUpWebBrowser(browser);
 		log.info("setUp()");
 	}
 
@@ -40,21 +42,27 @@ public class TestMenuPage {
 	@Test
 	public void testAboutUsVisibility() {
 		homePage.open();
-		Assert.assertEquals(homePage.driver.getCurrentUrl(), homePage.getEndavaURL(), "Incorrect URL!");
-		Assert.assertEquals(homePage.driver.getTitle(), homePage.getEndavaTitle(), "Incorrect title!");
+		Assert.assertEquals(homePage.driver.getCurrentUrl().toLowerCase(), homePage.getEndavaURL().toLowerCase(),
+				"Incorrect URL!");
+		Assert.assertEquals(homePage.driver.getTitle().toLowerCase(), homePage.getEndavaTitle().toLowerCase(),
+				"Incorrect title!");
 		Utils.webDriverWait(homePage.driver, homePage.getContactButtons());
 		menuPage = homePage.openMenu();
 		Utils.webDriverWait(menuPage.driver, menuPage.getNavigationList());
 		menuPage.clickOnInvestors();
+		Assert.assertEquals(menuPage.driver.getCurrentUrl(), "https://investors.endava.com/home/default.aspx",
+				"Incorrect URL for the INVESTORS page!");
+		Assert.assertEquals(homePage.driver.getTitle().toLowerCase(), "Investors".toLowerCase(),
+				"Incorrect title for Investors page!");
 		Utils.webDriverWait(menuPage.driver, menuPage.getInvestorsAboutUs());
 		Assert.assertTrue(menuPage.getTextFromElement(menuPage.getInvestorsAboutUs()).contains("ABOUT US"),
 				"Text \"ABOUT US\" not found!");
-		log.info("isAboutUsVisible() : VALIDATION SUCCESSFUL!");
+		log.info("testAboutUsVisibility() : VALIDATION SUCCESSFUL!");
 	}
 
 	@AfterTest
 	public void tearDown() {
-		menuPage.quit();		
+		menuPage.quit();
 		log.info("tearDown()");
 	}
 }
