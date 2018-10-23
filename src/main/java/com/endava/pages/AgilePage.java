55becd3127d8acd8509706f25980f21dd9672e78
gkukolj-endava <goran.kukolj@endava.com>
2018-10-23 14:35:27 +0200

    T8 added

--- src/main/java/com/endava/pages/AgilePage.java
+++ src/main/java/com/endava/pages/AgilePage.java
@@ -1,6 +1,8 @@
 package com.endava.pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -9,23 +11,144 @@ import org.openqa.selenium.WebDriver;
 
 public class AgilePage extends BasePage {
 
-    private static final String AGILE_URL = "https://www.endava.com/en/Agile";
-    private static final String AGILE_TITLE = "Agile";
-    private By agileOnRibbonMenu = By.xpath("//*[@id=\"secondary-nav\"]/ul/li[2]");
+	private By autofillWithLinkedinButton = By
+			.xpath("//*[contains(@id,'li_ui_li_gen')and contains(text(), 'Sign in with LinkedIn')]");
+	private By allowAccessButton = By.xpath("//*[@id=\"body\"]/div/form/div[2]/ul/li[1]/input");
+	private By emailInputField = By.xpath("//*[@id=\"session_key-oauthAuthorizeForm\"]");
+	private By cancelButton = By.xpath("//*[@id=\"body\"]/div/form/div[2]/ul/li[2]/button");
+	private By signInToLinkedinMessage = By.xpath("//*[@id=\"body\"]/div/form/div[1]/p");
+	private By enterValidEmailErrorMessage = By.xpath("//*[@id=\"session_key-oauthAuthorizeForm-error\"]");
+	private By enterPasswordErrorMessage = By.xpath("//*[@id=\"session_password-oauthAuthorizeForm-error\"]");
+	private By correctMarkedFieldsErrorMessage = By.xpath("//*[@id=\"global-error\"]/div/p/strong");
+	private By agileOnRibbonMenu = By.xpath("//*[@id=\"secondary-nav\"]/ul/li[2]");
+	private static final String POP_UP_WINDOW_TITLE = "Authorize | LinkedIn";
+	private static final String POP_UP_WINDOW_SUBMIT_URL = "https://www.linkedin.com/uas/oauth/authorize/submit";
+	private static final String SIGN_IN_TO_LINKEDIN = "Sign in to LinkedIn and allow access:";
+	private static final String ENTER_VALID_EMAIL = "Please enter a valid email address.";
+	private static final String ENTER_PASSWORD = "Please enter a password.";
+	private static final String CORRECT_MARKED_FIELDS = "Please correct the marked field(s) below.";
+	private static final String AGILE_URL = "https://www.endava.com/en/Agile";
+	private static final String AGILE_TITLE = "Agile";
+	private static Logger log = Logger.getLogger(AgilePage.class);
 
-    protected AgilePage(WebDriver driver) {
-        super(driver);
-    }
+	protected AgilePage(WebDriver driver) {
+		super(driver);
+	}
 
-    public By getAgileOnRibbonMenu() {
-        return agileOnRibbonMenu;
-    }
+	/**
+	 * Finds the autofill with linkedin button on the page and clicks on it
+	 * 
+	 * @author Goran.Kukolj
+	 */
+	public void clickOnAutofillWithLinkedinButton() {
+		driver.findElement(this.autofillWithLinkedinButton).click();
+		log.debug("Finds and clicks on autofill with linkedin button");
+	}
 
-    public String getAgileUrl() {
-        return AGILE_URL;
-    }
+	/**
+	 * Scrolls to the autofill with linkedin button
+	 * 
+	 * @author Goran.Kukolj
+	 */
+	public void scrollToautofillWithLinkedinButton() {
+		((JavascriptExecutor) driver).executeScript("scroll(0, 4800);");
+		log.debug("Scrolls to autofill with linkedin button");
+	}
 
-    public String getAgileTitle() {
-        return AGILE_TITLE;
-    }
+	/**
+	 * Finds input email field and enters invalid email
+	 * 
+	 * @author Goran.Kukolj
+	 */
+	public void enterEmailAddress() {
+		driver.findElement(emailInputField).sendKeys("inputSomething");
+		log.debug("Enters invalid email address");
+	}
+
+	/**
+	 * Finds allow access button and clicks on it
+	 * 
+	 * @author Goran.Kukolj
+	 */
+	public void clickOnAllowAccessButton() {
+		driver.findElement(this.allowAccessButton).click();
+		log.debug("Finds and clicks on allow access button");
+	}
+
+	/**
+	 * Finds cancel button and clicks on it
+	 * 
+	 * @author Goran.Kukolj
+	 */
+	public void clickOnCancelButton() {
+		driver.findElement(this.cancelButton).click();
+	}
+
+	public By getAgileOnRibbonMenu() {
+		return agileOnRibbonMenu;
+	}
+
+	public String getAgileUrl() {
+		return AGILE_URL;
+	}
+
+	public String getAgileTitle() {
+		return AGILE_TITLE;
+	}
+
+	public By getAutofillWithLinkedinButton() {
+		return autofillWithLinkedinButton;
+	}
+
+	public By getAllowAccessButton() {
+		return allowAccessButton;
+	}
+
+	public By getEmailInputField() {
+		return emailInputField;
+	}
+
+	public By getCancelButton() {
+		return cancelButton;
+	}
+
+	public By getSignInToLinkedinMessage() {
+		return signInToLinkedinMessage;
+	}
+
+	public By getEnterValidEmailErrorMessage() {
+		return enterValidEmailErrorMessage;
+	}
+
+	public By getEnterPasswordErrorMessage() {
+		return enterPasswordErrorMessage;
+	}
+
+	public By getCorrectMarkedFieldsErrorMessage() {
+		return correctMarkedFieldsErrorMessage;
+	}
+
+	public String getPopUpWindowTitle() {
+		return POP_UP_WINDOW_TITLE;
+	}
+
+	public String getPopUpWindowSubmitUrl() {
+		return POP_UP_WINDOW_SUBMIT_URL;
+	}
+
+	public String getSignInToLinkedin() {
+		return SIGN_IN_TO_LINKEDIN;
+	}
+
+	public String getEnterValidEmail() {
+		return ENTER_VALID_EMAIL;
+	}
+
+	public String getEnterPassword() {
+		return ENTER_PASSWORD;
+	}
+
+	public String getCorrectMarkedFields() {
+		return CORRECT_MARKED_FIELDS;
+	}
 }
--- src/main/java/com/endava/util/Utils.java
+++ src/main/java/com/endava/util/Utils.java
@@ -1,23 +1,23 @@
 package com.endava.util;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import com.endava.pages.HomePage;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Locatable;
 import org.openqa.selenium.interactions.internal.Coordinates;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.endava.pages.HomePage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
  * @author Vladimir Krekic
@@ -26,23 +26,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Utils {
 
 	private static Logger log = Logger.getLogger(Utils.class);
+
 	/**
 	 * @author Vladimir Krekic
 	 * @param browser String that represents chosen browser from testng.xml file
 	 * @return HomePage
 	 */
-	public static HomePage setUpWebBrowser(String browser){
+	public static HomePage setUpWebBrowser(String browser) {
 		HomePage homePage;
-		if(browser.equalsIgnoreCase("chrome")){
+		if (browser.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			homePage = new HomePage(new ChromeDriver());
-		}else if(browser.equalsIgnoreCase("firefox")){
+		} else if (browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			homePage = new HomePage(new FirefoxDriver());
-		}else if(browser.equalsIgnoreCase("internet explorer")){
+		} else if (browser.equalsIgnoreCase("internet explorer")) {
 			WebDriverManager.iedriver().setup();
 			homePage = new HomePage(new InternetExplorerDriver());
-		}else throw new RuntimeException();
+		} else
+			throw new RuntimeException();
 		log.debug("setUpWebBrowser(browser) - returns HomePage with chosen browser driver");
 		return homePage;
 	}
@@ -52,8 +54,7 @@ public class Utils {
 	 * @param locator
 	 */
 	public static void webDriverWait(WebDriver driver, By locator) {
-		new WebDriverWait(driver, 5)
-				.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
 
 	/**
@@ -69,14 +70,13 @@ public class Utils {
 	}
 
 	/**
-	 * @author Vladimir Krekic
-	 * Method is selecting (clicking on) WebElement
+	 * @author Vladimir Krekic Method is selecting (clicking on) WebElement
 	 * @param element WebElement
 	 * @return boolean
 	 */
-	public static boolean selectElement(WebElement element){
+	public static boolean selectElement(WebElement element) {
 		makeItVisible(element);
-		if(element.isDisplayed()){
+		if (element.isDisplayed()) {
 			element.click();
 			log.debug("WebElement clicked");
 			return true;
@@ -86,12 +86,43 @@ public class Utils {
 	}
 
 	/**
-	 * @author Vladimir Krekic
-	 * Makes web element visible
+	 * @author Vladimir Krekic Makes web element visible
 	 * @param webElement
 	 */
-	public static void makeItVisible(WebElement webElement){
+	public static void makeItVisible(WebElement webElement) {
 		Coordinates coordinates = ((Locatable) webElement).getCoordinates();
 		coordinates.inViewPort();
+	}
+
+	/**
+	 * @author Goran.Kukolj
+	 * @param driver
+	 * @param locator
+	 * @param originalText
+	 * @return true or false depending on comparing two strings
+	 */
+	public static boolean validateString(WebDriver driver, By locator, String originalText) {
+		WebElement element = driver.findElement(locator);
+		String textToCompare = element.getText();
+		log.debug("Compares two strings");
+		return textToCompare.equals(originalText);
+	}
+
+	/**
+	 * Transfers control to other window
+	 * 
+	 * @author Goran.Kukolj
+	 * @param driver
+	 */
+	public static void switchControlToNewWindow(WebDriver driver) {
+		String mainWindow = driver.getWindowHandle();
+		Set<String> windows = driver.getWindowHandles();
+		for (String window : windows) {
+			if (!window.equals(mainWindow)) {
+				driver.switchTo().window(window);
+				break;
+			}
+		}
+		log.debug("Transfers control to other window");
 	}
 }
